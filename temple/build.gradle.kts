27286ebba2ca9d32337a5042629737a046bd7f92
MMarijke@ <149050453+MMarijke@users.noreply.github.com>
2026-07-04 12:52:07 +0200

    Update Lombok and Error Prone versions in build.gradle.kts

--- temple/build.gradle.kts
+++ temple/build.gradle.kts
@@ -10,8 +10,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
-    id("io.freefair.lombok") version "8.0.1"
-    id("net.ltgt.errorprone") version "latest.release"
+    id("io.freefair.lombok") version "8.6"
 }
 
 
@@ -23,13 +22,12 @@ repositories {
 dependencies {
     // This dependency is used by the application.
     implementation("com.google.guava:guava:31.1-jre")
-    errorprone("com.google.errorprone:error_prone_core:latest.release")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(20))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
