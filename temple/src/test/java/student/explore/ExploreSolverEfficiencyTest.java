bece73ff5683e5c0b20dbe8b51011e8d0fae089a
Michael Schenk <103676550+MichaelSchenkDe@users.noreply.github.com>
2026-07-12 12:12:02 +0200

    Create ExploreSolverEfficiencyTest

---
+++ temple/src/test/java/student/explore/ExploreSolverEfficiencyTest.java
@@ -0,0 +1,42 @@
+package student.explore;
+
+import org.junit.jupiter.api.Test;
+import student.ExploreSolver;
+import student.explore.support.ExploreTestGraphs;
+import student.explore.support.MockExplorationState;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Checks that {@link ExploreSolver} does not waste moves on simple graphs
+ * where a single path leads to the orb.
+ */
+class ExploreSolverEfficiencyTest {
+
+    @Test
+    void usesMinimumMovesOnLinearGraph() {
+        MockExplorationState state = ExploreTestGraphs.linear();
+
+        ExploreSolver.solve(state);
+
+        assertEquals(3, state.moveHistory().size());
+    }
+
+    @Test
+    void usesMinimumMovesOnZigZagGraph() {
+        MockExplorationState state = ExploreTestGraphs.zigZag();
+
+        ExploreSolver.solve(state);
+
+        assertEquals(4, state.moveHistory().size());
+    }
+
+    @Test
+    void usesNoMovesWhenOrbIsAtStart() {
+        MockExplorationState state = ExploreTestGraphs.orbAtStart();
+
+        ExploreSolver.solve(state);
+
+        assertEquals(0, state.moveHistory().size());
+    }
+}
